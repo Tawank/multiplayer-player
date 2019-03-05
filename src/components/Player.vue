e18d71b4af917b2b66017a74340336f6a35bb5ad
Karol Stawowski <kstawowski@gmail.com>
2019-03-05 09:44:18 +0100

    added youtube player

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -5,17 +5,38 @@
       wrap
     >
       <v-flex xs12>
-        Player
+        Player<br>
+        {{database}}
+        <vue-plyr>
+          <div class="plyr__video-embed">
+            <iframe
+              :src="'https://www.youtube.com/embed/'+ database +'?iv_load_policy=3&modestbranding=1&playsinline=1&showinfo=0&rel=0&enablejsapi=1'"
+              allowfullscreen allowtransparency allow="autoplay">
+            </iframe>
+          </div>
+        </vue-plyr>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 export default {
-  data() {
+  data () {
     return {
+      database: ''
     }
+  },
+  mounted () {
+    firebase.database().ref('/').on('value', (snapshot) => {
+      this.database = snapshot.val()
+    })
+  },
+  methods: {
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
 import router from './router'
 import store from './store'
 
@@ -9,5 +13,15 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA0AhNeFdAuO_M0yZSwiH61nVY7RKTnTAM',
+      authDomain: 'multiplayer-player.firebaseapp.com',
+      databaseURL: 'https://multiplayer-player.firebaseio.com',
+      projectId: 'multiplayer-player',
+      storageBucket: 'multiplayer-player.appspot.com',
+      messagingSenderId: '401823794317'
+    })
+  }
 }).$mount('#app')
