fa15cff4789036b8cf11af049337f8981f3f4bc8
Tawank <kstawowski@gmail.com>
2019-05-13 09:59:15 +0200

    update

--- src/components/AddRoom.vue
+++ src/components/AddRoom.vue
@@ -2,21 +2,23 @@
   <v-layout>
     <v-flex xs12>
       <v-layout>
-        <v-flex xs4>
+        <v-flex xs5>
           <v-text-field
             v-model="roomName"
+            class="mx-2"
             label="Nazwa pokoju"
             solo
           ></v-text-field>
         </v-flex>
-        <v-flex xs4>
+        <v-flex xs5>
           <v-text-field
             v-model="roomDescription"
+            class="mx-2"
             label="Opis pokoju"
             solo
           ></v-text-field>
         </v-flex>
-        <v-flex xs4>
+        <v-flex xs1>
           <v-btn @click="addRoom">Dodaj pokój</v-btn>
         </v-flex>
       </v-layout>
